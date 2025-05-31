f9d7c6d4f02d8e3d435ef8a047d139291fb355f0
Hu-chih-yao <104767653+Hu-chih-yao@users.noreply.github.com>
2025-05-31 15:55:33 +0800

    Integrate SOAP Note and Update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,3 @@
-/**
- * Copyright 2024 Google LLC
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import { useRef, useState } from "react";
 import "./App.scss";
 import { LiveAPIProvider } from "./contexts/LiveAPIContext";
@@ -47,15 +31,21 @@ function App() {
             <SidePanel />
             <main>
               <div className="main-app-area">
-                <Altair />
-                <video
-                  className={cn("stream", {
-                    hidden: !videoRef.current || !videoStream,
-                  })}
-                  ref={videoRef}
-                  autoPlay
-                  playsInline
-                />
+                <div className="participants-view">
+                  <div className="webcam-participant">
+                    <video
+                      className={cn("stream", {
+                        hidden: !videoRef.current || !videoStream,
+                      })}
+                      ref={videoRef}
+                      autoPlay
+                      playsInline
+                    />
+                  </div>
+                  <div className="ai-participant">
+                    <Altair />
+                  </div>
+                </div>
                 <SoapNote isVisible={showSoapNote} />
               </div>
 
@@ -64,9 +54,7 @@ function App() {
                 supportsVideo={true}
                 onVideoStreamChange={setVideoStream}
                 enableEditingSettings={true}
-              >
-                {/* Custom controls can be added here */}
-              </ControlTray>
+              />
             </main>
           </div>
         </SoapNoteProvider>
